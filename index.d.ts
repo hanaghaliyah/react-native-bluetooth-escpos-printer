df2895830eb990bb96f2ef3d457aa3c8834ed233
hana.ghaliyah <hana.ghaliyah@kreators.com>
2023-10-09 14:40:14 +0700

    update index

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module '@brooons/react-native-bluetooth-escpos-printer' {
+declare module "@brooons/react-native-bluetooth-escpos-printer" {
   namespace BluetoothEscposPrinter {
     enum DEVICE_WIDTH {
       WIDTH_58 = 384,
@@ -6,10 +6,10 @@ declare module '@brooons/react-native-bluetooth-escpos-printer' {
     }
 
     enum ERROR_CORRECTION {
-      L =  1,
-      M =  0,
-      Q =  3,
-      H =  2,
+      L = 1,
+      M = 0,
+      Q = 3,
+      H = 2,
     }
 
     enum BARCODETYPE {
@@ -41,6 +41,11 @@ declare module '@brooons/react-native-bluetooth-escpos-printer' {
       widthtimes?: number; // Text font mul times in width. Default 0
       heigthtimes?: number; // Text font mul times in height. Default 0
       fonttype?: number; // Text font type. Default 0
+    }
+
+    interface IPrintQRCodeOptions {
+      size: number;
+      correctionLevel: number;
     }
 
     interface IPrintPicOptions {
@@ -97,7 +102,14 @@ declare module '@brooons/react-native-bluetooth-escpos-printer' {
       columnWidths: Array<number>,
       columnAligns: Array<ALIGN>,
       columnTexts: Array<string>,
-      options: IPrintTextOptions,
+      options: IPrintTextOptions
+    ): Promise<void>;
+
+    function printColumnQRCode(
+      columnWidths: Array<number>,
+      columnAligns: Array<ALIGN>,
+      columnTexts: Array<string>,
+      options: IPrintQRCodeOptions
     ): Promise<void>;
 
     function setWidth(width: number): Promise<void>;
@@ -109,13 +121,20 @@ declare module '@brooons/react-native-bluetooth-escpos-printer' {
      * width: the pic width, basic on devices width(dots, 58mm-384);
      * left: the left padding of the pic for the printing position adjustment.
      */
-    function printPic(base64encodeStr: string, options: IPrintPicOptions): Promise<void>;
+    function printPic(
+      base64encodeStr: string,
+      options: IPrintPicOptions
+    ): Promise<void>;
 
     function rotate(): Promise<void>;
 
     function setBlob(weight: number): Promise<void>;
 
-    function printQRCode(content: string, size: number, correctionLevel: number): Promise<void>;
+    function printQRCode(
+      content: string,
+      size: number,
+      correctionLevel: number
+    ): Promise<void>;
 
     function printBarCode(
       str: string,
@@ -123,10 +142,14 @@ declare module '@brooons/react-native-bluetooth-escpos-printer' {
       nWidthX: number,
       nHeight: number,
       nHriFontType: number,
-      nHriFontPosition: number,
+      nHriFontPosition: number
     ): Promise<void>;
 
-    function openDrawer(nMode: number, nTime1: number, nTime2: number): Promise<void>;
+    function openDrawer(
+      nMode: number,
+      nTime1: number,
+      nTime2: number
+    ): Promise<void>;
 
     function cutOnePoint(): Promise<void>;
   }
@@ -180,54 +203,54 @@ declare module '@brooons/react-native-bluetooth-escpos-printer' {
     }
 
     enum BARCODETYPE {
-      CODE128 = '128',
-      CODE128M = '128M',
-      EAN128 = 'EAN128',
-      ITF25 = '25',
-      ITF25C = '25C',
-      CODE39 = '39',
-      CODE39C = '39C',
-      CODE39S = '39S',
-      CODE93 = '93',
-      EAN13 = 'EAN13',
-      EAN13_2 = 'EAN13+2',
-      EAN13_5 = 'EAN13+5',
-      EAN8 = 'EAN8',
-      EAN8_2 = 'EAN8+2',
-      EAN8_5 = 'EAN8+5',
-      CODABAR = 'CODA',
-      POST = 'POST',
-      UPCA = 'EAN13',
-      UPCA_2 = 'EAN13+2',
-      UPCA_5 = 'EAN13+5',
-      UPCE = 'EAN13',
-      UPCE_2 = 'EAN13+2',
-      UPCE_5 = 'EAN13+5',
-      CPOST = 'CPOST',
-      MSI = 'MSI',
-      MSIC = 'MSIC',
-      PLESSEY = 'PLESSEY',
-      ITF14 = 'ITF14',
-      EAN14 = 'EAN14',
+      CODE128 = "128",
+      CODE128M = "128M",
+      EAN128 = "EAN128",
+      ITF25 = "25",
+      ITF25C = "25C",
+      CODE39 = "39",
+      CODE39C = "39C",
+      CODE39S = "39S",
+      CODE93 = "93",
+      EAN13 = "EAN13",
+      EAN13_2 = "EAN13+2",
+      EAN13_5 = "EAN13+5",
+      EAN8 = "EAN8",
+      EAN8_2 = "EAN8+2",
+      EAN8_5 = "EAN8+5",
+      CODABAR = "CODA",
+      POST = "POST",
+      UPCA = "EAN13",
+      UPCA_2 = "EAN13+2",
+      UPCA_5 = "EAN13+5",
+      UPCE = "EAN13",
+      UPCE_2 = "EAN13+2",
+      UPCE_5 = "EAN13+5",
+      CPOST = "CPOST",
+      MSI = "MSI",
+      MSIC = "MSIC",
+      PLESSEY = "PLESSEY",
+      ITF14 = "ITF14",
+      EAN14 = "EAN14",
     }
     enum FONTTYPE {
-      FONT_1 = '1',
-      FONT_2 = '2',
-      FONT_3 = '3',
-      FONT_4 = '4',
-      FONT_5 = '5',
-      FONT_6 = '6',
-      FONT_7 = '7',
-      FONT_8 = '8',
-      SIMPLIFIED_CHINESE = 'TSS24.BF2',
-      TRADITIONAL_CHINESE = 'TST24.BF2',
-      KOREAN = 'K',
+      FONT_1 = "1",
+      FONT_2 = "2",
+      FONT_3 = "3",
+      FONT_4 = "4",
+      FONT_5 = "5",
+      FONT_6 = "6",
+      FONT_7 = "7",
+      FONT_8 = "8",
+      SIMPLIFIED_CHINESE = "TSS24.BF2",
+      TRADITIONAL_CHINESE = "TST24.BF2",
+      KOREAN = "K",
     }
     enum EEC {
-      LEVEL_L = 'L',
-      LEVEL_M = 'M',
-      LEVEL_Q = 'Q',
-      LEVEL_H = 'H',
+      LEVEL_L = "L",
+      LEVEL_M = "M",
+      LEVEL_Q = "Q",
+      LEVEL_H = "H",
     }
     enum ROTATION {
       ROTATION_0 = 0,
@@ -259,8 +282,8 @@ declare module '@brooons/react-native-bluetooth-escpos-printer' {
       SPEED4 = 4,
     }
     enum TEAR {
-      ON = 'ON',
-      OFF = 'OFF',
+      ON = "ON",
+      OFF = "OFF",
     }
     enum READABLE {
       DISABLE = 0,
@@ -273,8 +296,8 @@ declare module '@brooons/react-native-bluetooth-escpos-printer' {
       gap?: number;
       direction?: DIRECTION;
       reference?: [number, number];
-      tear?: TEAR,
-      sound?: number,
+      tear?: TEAR;
+      sound?: number;
       text?: Array<{
         text: string;
         x: number;
@@ -304,8 +327,8 @@ declare module '@brooons/react-native-bluetooth-escpos-printer' {
       image?: Array<{
         x: number;
         y: number;
-        mode: BITMAP_MODE,
-        width: number,
+        mode: BITMAP_MODE;
+        width: number;
         image: string;
       }>;
     }
